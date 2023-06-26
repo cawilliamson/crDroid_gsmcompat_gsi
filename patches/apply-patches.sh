8c6f3af13f4a6a4cfc1847e443bb2158cf832db0
Christopher A. Williamson <home@chrisaw.com>
2023-06-26 10:28:07 +0100

    Removed unnecessary patch and moved apply patches

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-patches="$(readlink -f -- $1)"
-tree="$2"
+patches_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+tree="${1}"
 
-for project in $(cd $patches/patches/$tree; echo *); do
+for project in $(cd $patches_dir/$tree; echo *); do
     p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
     [ "$p" == build ] && p=build/make
     [ "$p" == treble/app ] && p=treble_app
     [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
     pushd $p &>/dev/null
-    for patch in $patches/patches/$tree/$project/*.patch; do
+    for patch in $patches_dir/$tree/$project/*.patch; do
         git am $patch || exit
     done
     popd &>/dev/null
