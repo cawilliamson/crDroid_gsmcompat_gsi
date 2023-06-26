8b0857831b2afc4630020ce09c770be18ffe54a2
Christopher A. Williamson <home@chrisaw.com>
2023-06-26 10:36:18 +0100

    Update build.sh and fix manifest remotes

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-docker build -t chrisawcom/buildrom .
+docker build -t chrisawcom/buildrom . 
+
+# fetch rom sources
+
+# docker run -it -v $(pwd):/src chrisawcom/buildrom
+# mkdir -p /src/build
+# cd /src/build
+# repo init -u https://github.com/GrapheneOS/platform_manifest.git
+# mkdir -p .repo/local_manifests
+# cp -v ../manifest.xml .repo/local_manifests/
+# repo sync -c --force-sync --no-clone-bundle --no-tags -j4
+
+# apply trebledroid patches
+
+# bash patches/apply_patches.sh trebledroid
