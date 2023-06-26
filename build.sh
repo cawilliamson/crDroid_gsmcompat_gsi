4b795850fc81cb4d3cb933f8f6f6c061bdc893be
Christopher A. Williamson <home@chrisaw.com>
2023-06-26 16:21:19 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
 docker build -t chrisawcom/buildrom . 
-=======
-docker build -t chrisawcom/buildrom .
->>>>>>> 6ff9c0e (Remove unused patches and tidy up manifest)
 
 # fetch rom sources
 
 # docker run -it -v $(pwd):/src chrisawcom/buildrom
 # mkdir -p /src/build
 # cd /src/build
-# repo init -u https://github.com/GrapheneOS/platform_manifest.git
+# repo init -u https://github.com/GrapheneOS/platform_manifest.git -b 13
 # mkdir -p .repo/local_manifests
 # cp -v ../manifest.xml .repo/local_manifests/
 # repo sync -c --force-sync --no-clone-bundle --no-tags -j4
@@ -19,3 +15,17 @@ docker build -t chrisawcom/buildrom .
 # apply trebledroid patches
 
 # bash patches/apply_patches.sh trebledroid
+
+# build treble app
+
+# pushd treble_app/
+# bash build.sh release
+# popd
+# mkdir -p ../vendor/hardware_overlay/
+# cp -v treble_app/TrebleApp.apk vendor/hardware_overlay/TrebleApp/app.apk
+
+# build rom
+
+# source script/envsetup.sh
+# choosecombo release aosp_arm64 user
+# make -j$(nproc --all) systemimage
